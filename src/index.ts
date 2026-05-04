6022e1edef7d8f4a69c1e11f03c766b2c7ed8778
Lobo <lobomfz@protonmail.com>
2026-05-04 18:14:17 -0300

    remove playback commands

--- src/index.ts
+++ src/index.ts
@@ -10,13 +10,11 @@ import { ExportCommand } from '@/commands/export'
 import { InfoCommand } from '@/commands/info'
 import { InitCommand } from '@/commands/init'
 import { LibraryCommand } from '@/commands/library'
-import { PlayCommand } from '@/commands/play'
 import { ReleasesCommand } from '@/commands/releases'
 import { ScanCommand } from '@/commands/scan'
 import { SearchCommand } from '@/commands/search'
 import { StatusCommand } from '@/commands/status'
 import { SubtitlesCommand } from '@/commands/subtitles'
-import { WaitForCommand } from '@/commands/wait-for'
 
 import pkg from '../package.json'
 
@@ -30,7 +28,6 @@ const cli = await createCLI({
 cli.command(InitCommand)
 cli.command(InfoCommand)
 cli.command(LibraryCommand)
-cli.command(PlayCommand)
 cli.command(ScanCommand)
 cli.command(SearchCommand)
 cli.command(ReleasesCommand)
@@ -38,7 +35,6 @@ cli.command(DownloadCommand)
 cli.command(ExportCommand)
 cli.command(StatusCommand)
 cli.command(SubtitlesCommand)
-cli.command(WaitForCommand)
 
 await cli.run().catch((error) => {
   console.error(formatRpcError(error).message)
